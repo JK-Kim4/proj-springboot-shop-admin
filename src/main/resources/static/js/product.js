08fe9a87be07b06b5063114f20b29a3cb4e502d3
jongwan <jw.kim@changbi.com>
2022-10-04 13:25:11 +0900

    - 상품(구독권) CRUD
    Date: 2022.10.04
    Author: KJW

--- src/main/resources/static/js/product.js
+++ src/main/resources/static/js/product.js
@@ -1,10 +1,77 @@
 let main = {
     init : function (){
         let _this = this;
+        
+        /*파일 업로드*/
+        $("#uploadBtn").on("click", function (){
+           _this.upload();
+        });
 
+        /*상품 등록*/
+        $("#productInsertBtn").on("click", function (){
+           _this.insert();
+        });
 
+    },
+    insert: function (){
 
+    },
+    upload: function (){
 
+        let inputFile = $("#inputFile")[0].files[0];
+        let formData = new FormData();
+        let html = "";
+
+        if(inputFile == null || inputFile == undefined){
+            alert("업로드할 파일을 선택해 주세요");
+            return;
+        }
+
+        new Compressor(inputFile, {
+            quality: 0.8,
+            convertSize : 5000000,
+            success : function (result){
+                formData.append("files", new File([result], inputFile.name));
+                $.ajax({
+                    type : "POST",
+                    url : "/file/upload/image",
+                    enctype : "multipart/form-data",
+                    data : formData,
+                    processData: false,
+                    contentType: false,
+                    cache: false,
+                    success : function (data){
+
+                        if(data.resultCd = '0000'){
+                            alert("파일 업로드 성공 : " + data.resultMsg);
+
+                            $('#loading').hide();
+                            $("#uploadBtn").hide();
+                            $("#productImageFile").val(data.resultMsg);
+                            $("#productImageFileName").val(data.orgFileNm);
+                            $("#uploadValid").val("Y");
+                            html += "<img src='" + data.resultMsg + "' alt='uploadImage' style='max-width: 150px;'>";
+                            $("#imgControl").html(html);
+                        }else{
+                            alert("파일 업로드 실패, 다시 시도해 주세요.");
+                            $("#uploadValid").val("N");
+                            $("#productImageFile").val("");
+                            $("#productImageFileName").val("");
+                        }
+                    },
+                    error : function (data){
+                        alert("시스템 에러 발생. 지속적인 오류 발생 시 관리자에게 문의해 주세요");
+                        $("#uploadValid").val("N");
+                        $("#productImageFile").val("");
+                        $("#productImageFileName").val("");
+                    }
+                });
+            },
+            error : function (){
+                console.log('resize error');
+                return;
+            }
+        });
     },
     getProductList : function (pageNum, pageSize){
         $.ajax({
